074b9a6d36eda58e2c353ac0e463a5b6da34e540
KorotkovK <korotkov.kostya123@gmail.com>
2023-08-26 01:29:39 +0300

    main

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+    .map(
+        item => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `
+    )
+    .join('');
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// Ініціалізація бібліотеки SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+// Обробник події кліку на зображенні (для прикладу)
+galleryList.addEventListener('click', event => {
+    event.preventDefault();
+});
